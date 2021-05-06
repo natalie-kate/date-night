a89a0fcabcef989258e14cb39f21b2478b8990ad
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2021-05-06 19:07:32 +0000

    Send mail function

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,17 +1,48 @@
-let sendForm = document.getElementById("contact-form");
+const sendForm = document.getElementById("contact-form");
 sendForm.addEventListener('submit', contactForm);
-let response = document.getElementById("response");
-let submitbutton = document.getElementById("submit-button");
+const response = document.getElementById("response");
+const submitbutton = document.getElementById("submit-button");
+const success = document.getElementById("submit-modal");
+
 
 function contactForm(event) {
     event.preventDefault();
     let firstName = sendForm.elements[1].value;
     let email = sendForm.elements[3].value;
     let comment = sendForm.elements[4].value;
+    let secondName = sendForm.elements[2].value;
     let responseText = `
     <h4>Thanks ${firstName}</h4>
-    <p> The message submitted was: "${comment}"<br>
+    <p>The message submitted was: "${comment}"<br>
     We'll get back to you as soon as possible via ${email}.<br>
     Have an amazing Date night! <i class="fas fa-heart"</p>`;
-    response.innerHTML = responseText;
-}
+    response.innerHTML = responseText;  
+
+    submitbutton.innerText = "Sent!"
+     
+    $('#submit-modal').modal('show');
+
+    return sendMail(this);
+
+    function sendMail (sendForm) {
+        emailjs.init("user_wAOlLN2zYLGpP3C5ZlKTc");
+        emailjs.send("service_i1srpob","date-night", {
+            "first-name" : firstName,
+            "second-name" : secondName,
+            "email-address" : email,
+            "comment" : comment
+        }); 
+    }
+}  
+
+
+
+
+
+
+
+
+
+
+
+
