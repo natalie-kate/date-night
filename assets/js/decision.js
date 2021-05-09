1578d015b0bd097093ec4a7a8f56f8f9acad3a31
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2021-05-09 18:25:27 +0000

    Added in conditions to target styling

--- assets/js/decision.js
+++ assets/js/decision.js
@@ -9,9 +9,20 @@ function timer() {
             return;
         } else {
         $("#timer p").text(timeLeft);
-        } timeLeft -= 1 
+        $("#timer p").css("color","orange");
+
+        if (timeLeft <= 3) {
+        $("#timer p").css("transition-duration","2000ms");
+        $("#timer p").css("color","red");
+        }
+        if (timeLeft < 2) {
+            $("#timer p").css("animation", "flash 500ms 5")
+        }
+    }
+        timeLeft -= 1 
     }, 1000)  
 }
+    
     
 
      
