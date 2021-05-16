2f92cc9de5af1333e708b54e03aef8c8173a989b
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2021-05-16 18:47:27 +0000

    Split them out into smaller functions.

--- assets/js/decision.js
+++ assets/js/decision.js
@@ -17,68 +17,66 @@ function timer() {
                 $("#timer p").css("color", "red");
             }
             if (timeLeft < 2) {
-                $("#timer p").css("animation", "flash 500ms 5")
+                $("#timer p").css("animation", "flash 500ms 5");
             }
         }
-        timeLeft -= 1
+        timeLeft -= 1;
     }, 1000)
 }
 
-$('#option1').click(function() {
+$("#option1").click(function() {
+    let stayingInOptions = ["Movie Night", "TV Binge", "Takeaway", "Game Night", "Cook Together", "Video Games", "Cocktail Making", "Pamper Session"];
+    $(this).hide();
+    $("#option2").hide();
+    $("#option3").show();
+    $("#option4").show();
+    game(stayingInOptions);
+  });
 
-    $("#option1").attr('id', 'option3');
-    $("#option2").attr('id', 'option4');
-    const stayInOptions = ["Movie Night", "TV Binge", "Takeaway", "Game Night", "Cook Together", "Video Games", "Cocktail Making", "Pamper Session"];
-    randomOptions(); 
+$("#option2").click(function() {
+    let goOutOptions = ["Bowling", "Dinner", "Club", "Cinema", "Bar", "Escape Room", "Live Music", "Pool Hall"];
+    $(this).hide();
+    $("#option1").hide();
+    $("#option3").show();
+    $("#option4").show();
+    game(goOutOptions);
+  });
 
-    function randomOptions() {
-    let num1 = Math.floor(Math.random()*stayInOptions.length);
-    let button1 = stayInOptions.splice([num1],1);
-    let num2 = Math.floor(Math.random()*stayInOptions.length);
-    let button2 = stayInOptions.splice([num2],1);
-    let chosenOptions = [];
-    
-    $("#option3").text(button1);
-    $("#option4").text(button2);
+function game(options) {
+   let chosenOptions = [];
 
-    $("#option3").click(function() {
-        let chosenOption = $("#option3").text();
-        console.log(chosenOption);
-        randomOptions();
-    });
-    $("#option4").click(function() {
-        chosenOptions.push($("#option3").text())
-        randomOptions();
-    });
-    console.log(chosenOptions);
-}});
 
-$('#option2').click(function() {
+   randomChoice(options)
 
-    $("#option1").attr('id', 'option5');
-    $("#option2").attr('id', 'option6');
-    
-    const goOutOptions = ["Bowling", "Dinner", "Club", "Cinema", "Bar", "Escape Room", "Live Music", "Pool Hall"];
+function randomChoice(optionsArray){
+  length = optionsArray.length;
+  randomNumber1 = Math.floor(Math.random()*length);
+  randomNumber2 = Math.floor(Math.random()*length);
 
+  getOptions(randomNumber1, randomNumber2);
+
+function getOptions(num1, num2) {
+  button1 = options[num1];
+  options.splice([num1],1);
+  button2 = options[num2];
+  options.splice([num2],1)
+
+  displayOptions(button1, button2);
+}
+
+function displayOptions (buttonText1, buttonText2) {
+  $("#option3").text(buttonText1);
+  $("#option4").text(buttonText2);
+};
+}
+$("#option3").click(function() {
+    chosenOptions.push($(this).text())
     randomOptions();
+});
 
-    function randomOptions() {
-        let num1 = Math.floor(Math.random()*goOutOptions.length);
-        let button1 = goOutOptions.splice([num1],1);
-        let num2 = Math.floor(Math.random()*goOutOptions.length);
-        let button2 = goOutOptions.splice([num2],1);
-        let chosenOptions = [];
-        
-        $("#option5").text(button1);
-        $("#option6").text(button2);
-    
-        $("#option5").click(function() {
-            let chosenOption = $("#option5").text();
-            console.log(chosenOption);
-            randomOptions();
-        });
-        $("#option6").click(function() {
-            chosenOptions.push($("#option6").text())
-            randomOptions();
-        });
-}});
+$("#option4").click(function() {
+    chosenOptions.push($(this).text());
+    randomOptions();
+});
+};
+
